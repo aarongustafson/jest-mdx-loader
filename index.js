47399682aa0dd90dbbe0bdb3db15a43ea01b2390
Aaron Gustafson <aaron@easy-designs.net>
2024-05-20 09:16:03 -0700

    ESM conversion

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-"use strict";
-
 import createTransformer from "./src/createTransformer";
 
-module.exports = {
-  process: createTransformer()
-};
+const process = createTransformer();
+
+export { process };
